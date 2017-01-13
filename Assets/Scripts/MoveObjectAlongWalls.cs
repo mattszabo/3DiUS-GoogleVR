9eebca00c8d4d0c9766c26331bd931716b6fa17b
Matt Szabo <szabo.matthew@gmail.com>
2017-01-13 17:06:59 +1100

    allow movement of profile pic from back wall to left wall with issues

--- Assets/Scripts/MoveObjectAlongWalls.cs
+++ Assets/Scripts/MoveObjectAlongWalls.cs
@@ -7,8 +7,17 @@ namespace Moveable {
 
 public class MoveObjectAlongWalls : MonoBehaviour {
 
-
 	public PointerType pointerType;
+
+	public GameObject frontWall;
+	public GameObject rightWall;
+	public GameObject backWall;
+	public GameObject leftWall;
+	public GameObject ceiling;
+	public GameObject floor;
+
+	public GameObject currentWall;
+
 	private GameObject pointer;
 
 	private bool isPickedUp;
@@ -47,14 +56,40 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 	}
 
 	private void FollowPointerAlongWall() {
-		Ray ray = new Ray (pointer.transform.position, pointer.transform.forward);
-//		GameObject camera = GameObject.Find ("Main Camera");
-		GameObject backWall = GameObject.Find ("BackWall");
-		transform.position = ray.GetPoint (Vector3.Distance(ray.origin, backWall.transform.position));
-		transform.position = new Vector3(transform.position.x, transform.position.y, backWall.transform.position.z - 0.3f);
-		transform.forward = GameObject.Find("BackWall").transform.forward;
-		transform.right = -GameObject.Find("BackWall").transform.right;
 
+		Ray pointerRay = new Ray (pointer.transform.position, pointer.transform.forward);
+
+//		// we pick up objects at their center. so get the distance from the center
+//		// to the edge and make sure we don't let the edge go through adjacent walls
+//		Vector3 
+
+		if (currentWall == backWall || currentWall == frontWall) {
+
+			// place the object along the wall in a position relative to the pointer
+			transform.position = pointerRay.GetPoint (Vector3.Distance (pointerRay.origin, currentWall.transform.position));
+			// adjust the z position to match the z pos of the current wall (rotating around the pointers origin also moves the z position, which we want to keep the same as the wall)
+			transform.position = new Vector3 (transform.position.x, transform.position.y, currentWall.transform.position.z - 0.3f);
+
+			// set the forward and right position of the object.
+			// when picking up with the pointer ray, we need to make sure these are set or the
+			// object we're holding can be flipped/rotated
+			transform.forward = currentWall.transform.forward;
+			transform.right = -currentWall.transform.right;
+
+			// move to left wall
+			float halfWidth = currentWall.GetComponent<Renderer>().bounds.size.x / 2.0f;
+			if (transform.position.x < currentWall.transform.position.x - halfWidth) {
+				Debug.Log(currentWall.GetComponent<Renderer>().bounds.size.x);
+				currentWall = leftWall;
+			}
+
+		} else if (currentWall == leftWall || currentWall == rightWall) {
+			transform.forward = -currentWall.transform.forward;
+			transform.right = -currentWall.transform.right;
+
+			transform.position = pointerRay.GetPoint (Vector3.Distance (pointerRay.origin, currentWall.transform.position));
+			transform.position = new Vector3 (currentWall.transform.position.x + 0.3f, transform.position.y, transform.position.z);
+		}
 
 		// 1. determine which wall the camera is facing
 		// 2. set object position at the back wall in relation to the camera (set x, y pos)
