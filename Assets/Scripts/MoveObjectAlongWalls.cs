8ef98577072c2a73d41906d3f1165ca6a96a4c57
Matt Szabo <szabo.matthew@gmail.com>
2017-02-12 15:40:03 +1100

    updated code to use an event driven state approach

--- Assets/Scripts/MoveObjectAlongWalls.cs
+++ Assets/Scripts/MoveObjectAlongWalls.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
 public class MoveObjectAlongWalls : MonoBehaviour {
 
@@ -13,7 +11,7 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 
 	private bool isPointedAt;
 	private bool isPickedUp;
-	private bool isPositionOnWallInitialised;
+	private bool isPointerPointingAtWall;
 
 	private Vector3 objectLastPosition;
 	private Vector3 objectVelocity;
@@ -22,8 +20,7 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 	{
 		NONE,
 		PICKED_UP,
-		UPDATE_OBJECT_POSITION,
-        AT_WALL_EDGE
+        NOT_PICKED_UP
     }
 
 	ObjectStates objectState;
@@ -32,120 +29,51 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 	readonly int layerPickedUpObject = 1 << 8;
 	int allLayersExceptPickedUpObject;
 
-	GameObject[] walls;
-
-	void OnEnable()
-	{
-		WallCollision.OnEnter += WallDetection;
+	void OnEnable() {
+		WallCollision.OnEnter += PointingAtWall;
 		WallCollision.OnExit += NotPointingAtWall;
 	}
 
-	void OnDisable()
-	{
-		WallCollision.OnEnter -= WallDetection;
+	void OnDisable() {
+		WallCollision.OnEnter -= PointingAtWall;
 		WallCollision.OnExit -= NotPointingAtWall;
-	}
-
-	void WallDetection(GameObject wall)
-	{
-		// switch(objectState) {
-		// 	case ObjectStates.PICKED_UP:
-		// 		if (wall != currentWall) {
-		// 			AlignObjectToWall(wall);
-		// 			SetCurrentWall(wall);
-		// 		}
-		// 		FollowPointerAlongWall ();
-		// 		break;
-		// 	default:
-		// 		break;
-		// }
-		Debug.Log("OMG WALL!");
-		if (wall != this.currentWall) {
-			this.currentWall = wall;
-		}
-	}
-
-    void NotPointingAtWall(GameObject gameObject)
-	{
-		Debug.Log("no wall");
-		objectState = ObjectStates.AT_WALL_EDGE;  
 	}
 
 	void Start () {
 		isPickedUp = false;
-		isPositionOnWallInitialised = false;
 		laserPointer = GameObject.Find ("Laser");
 		objectState = ObjectStates.NONE;
-
-		walls = GameObject.FindGameObjectsWithTag("Wall");
-
+		
 		// layer mask to select any object except those with layer set to PickedUpObject
 		allLayersExceptPickedUpObject = ~layerPickedUpObject;
 	}
 		
 	void Update () {
-
 		switch (objectState) {
 			case ObjectStates.NONE:
 				break;
-			case ObjectStates.AT_WALL_EDGE:
-				break;
 			case ObjectStates.PICKED_UP:
-				Debug.Log("PICKED UP, YOOOOO!");
+				if(isPointerPointingAtWall) {
+					FollowPointerAlongWall();
+				}
+				break;
+			case ObjectStates.NOT_PICKED_UP:
+				setNone();
 				break;
 			default:
 				break;
 		}
-
-		if (isPickedUp) {
-			
-		}
-	}
-
-	public void PickUpObject() {
-		isPickedUp = true;
 	}
 
 	private void FollowPointerAlongWall() {
+        Vector3 controllerOffset = new Vector3(0.35f, -1.0f, 0.0f);
+        Ray laserPointerRay = new Ray (laserPointer.transform.position + controllerOffset, laserPointer.transform.forward);
+		RaycastHit objectBehindPickedUpObject;
 
-        Vector3 controllerOffset = new Vector3(0.0f, 1.65f, 0.0f);
-        Ray laserPointerRay = new Ray (laserPointer.transform.position - controllerOffset, laserPointer.transform.forward);
-		RaycastHit hit = GetHitOfObjectBeingPointedAt (laserPointerRay);
-
-		// ignore the picked up object being hit so we can check for wall collisions
-		// if(isPickedUp) {
-		bool ignorePickedUpObject = true;
-		hit = GetHitOfObjectBeingPointedAt(laserPointerRay, ignorePickedUpObject);
-		// }
-			
-		// if (updatePosition) {
-//			UpdateObjectPositionToHitPoint (laserPointerRay, hit.point);
-		// }
-
-//		foreach (GameObject wall in walls) {
-//			if (IsAWallTagHit (hit, wall)) {
-////				AlignObjectToWall (wall);
-//			}
-//		}
-	}
-
-    public void FollowPointer() {
-        isPickedUp = true;
-    }
-
-    public void UnfollowPointer(){
-        isPickedUp = false;
-    }
-
-	private RaycastHit GetHitOfObjectBeingPointedAt(Ray pointerRay, bool ignorePickedUpObject = false) {
-		RaycastHit hit;
-		if (ignorePickedUpObject) {
-			Physics.Raycast (pointerRay, out hit, 140.0f, allLayersExceptPickedUpObject);
-		} else {
-			Physics.Raycast (pointerRay, out hit, 140.0f);
+		if(Physics.Raycast (laserPointerRay, out objectBehindPickedUpObject, 140.0f, allLayersExceptPickedUpObject) &&
+				objectBehindPickedUpObject.collider.CompareTag("Wall")) {
+			UpdateObjectPositionToHitPoint (laserPointerRay, objectBehindPickedUpObject.point);
 		}
-
-		return hit;
 	}
 
 	private bool IsObjectAlignedToAWall() {
@@ -159,32 +87,37 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 	}
 
 
-	private void AlignObjectToWall(GameObject wall) {
+	private void PointingAtWall(GameObject wall) {
+		isPointerPointingAtWall = true;
+		
 		if (this.currentWall != wall) {
-            this.currentWall = wall;
-
-			transform.right = this.currentWall.transform.right;
-
-            Quaternion alignedWallRotation = this.currentWall.transform.rotation;
-
-            if (transform.rotation != alignedWallRotation) {
-				Quaternion w = alignedWallRotation;
-                transform.rotation.Set(w.x, w.y, w.z, w.w);
-                //transform.rotation = alignedWallRotation;
-
-            }
+			AlignObjectToWall(wall);
 		}
 	}
 
-	private bool IsAWallTagHit(RaycastHit hit, GameObject wall) {
-		return hit.collider.gameObject == wall;
+	public void AlignObjectToWall(GameObject wall) {
+		transform.right = wall.transform.right;
+
+		Quaternion alignedWallRotation = wall.transform.rotation;
+		if (transform.rotation != alignedWallRotation) {
+			Quaternion w = alignedWallRotation;
+			transform.rotation = alignedWallRotation;
+		}
+		this.currentWall = wall;
+	}
+
+	public void NotPointingAtWall() {
+		isPointerPointingAtWall = false;
 	}
 		
+	public void setNone() {
+		objectState = ObjectStates.NONE;
+	}
 	public void SetPointedAt() {
 		isPointedAt = true;
 	}
 
-	public void SetUnpointedAt() {
+	public void SetNotPointedAt() {
 		isPointedAt = false;
 	}
 
@@ -192,7 +125,7 @@ public class MoveObjectAlongWalls : MonoBehaviour {
 		objectState = ObjectStates.PICKED_UP;
 	}	
 
-	public void setNone() {
-		objectState = ObjectStates.NONE;
+	public void setNotPickedUp() {
+		objectState = ObjectStates.NOT_PICKED_UP;
 	}
 }
--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -1,29 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.EventSystems;
 
-// attach me to a wall
+/* attach me to a wall */
 public class WallCollision : MonoBehaviour {
 
 	public delegate void Collided(GameObject wall);
+	public delegate void NotCollided();
 	public static event Collided OnEnter;
-	public static event Collided OnExit;
+	public static event NotCollided OnExit;
 
-	void OnCollisionEnter(Collision col)
-	{
-		Debug.Log ("on collision enter");
-		Debug.Log (col.gameObject.layer);
-		// if (col.gameobject.layer == "laser")
-
-		// this is 'on' the wall, we want to pass ourselves in the event, we tell the laser, you hit me!
-		// OnCollidedEnter(gameObject);
+	public void OnCollisionEnter(object gameObj) {
+		OnEnter(gameObject);
 	}
 
-	void OnCollisionExit(Collision col)
-	{
-		Debug.Log ("on collision exit");
-		Debug.Log (col.gameObject.layer);
-		// this is 'on' the wall, we want to pass ourselves in the event, we tell the laser, you hit me!
-		// OnCollidedExit(gameObject);
+	void OnCollisionExit() {
+		OnExit();
 	}
 }
