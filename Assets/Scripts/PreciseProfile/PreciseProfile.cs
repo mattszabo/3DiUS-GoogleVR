72774016e57fb6f5117a5d059de24931c346af73
Matt szabo <mattszabo@users.noreply.github.com>
2017-04-09 11:43:40 +1000

    Refactor selector, fix broken filtering logic

--- Assets/Scripts/PreciseProfile/PreciseProfile.cs
+++ Assets/Scripts/PreciseProfile/PreciseProfile.cs
@@ -1,39 +1,35 @@
-﻿/*
-DISPLAY A PRECISE PROFILE
-*/
-
+﻿using System.Text.RegularExpressions;
 using UnityEngine;
-using System.Text.RegularExpressions;
 
 public class PreciseProfile : MonoBehaviour {
 
 	private enum PreciseProfileSections {
-		ProfileName, ProfileTitle, ProfilePicture, ProfileBio
+		ProfileName, ProfileTitle, ProfileBio
 	};
 
-	private PreciseProfileModel profileModel;
+	private PreciseProfileModel _profileModel;
 
 	public void Init(PreciseProfileModel profile) {
-		profileModel = profile;
+		_profileModel = profile;
 		SetProfilePicture (profile.profilePictureTex);
 		SetProfileObjectText (profile.name, PreciseProfileSections.ProfileName.ToString());
 		SetProfileObjectText (profile.title, PreciseProfileSections.ProfileTitle.ToString());
-		string bioString = profile.bio.Substring(0, 265);
+		var bioString = profile.bio.Substring(0, 265);
 		bioString = Regex.Replace(bioString, ".{40}", "$0\n");
 		SetProfileObjectText (bioString, PreciseProfileSections.ProfileBio.ToString());
 	}
 
-	private void SetProfilePicture(Texture2D tex) {
-		GameObject profilePicture = transform.FindChild("ProfilePicture").gameObject;
+	private void SetProfilePicture(Texture tex) {
+		var profilePicture = transform.FindChild("ProfilePicture").gameObject;
 		profilePicture.GetComponent<Renderer> ().material.mainTexture = tex;
 	}
 
 	private void SetProfileObjectText(string objectText, string objectLabel) {
-		GameObject profileBio = transform.FindChild(objectLabel+"Card").FindChild(objectLabel).gameObject;
+		var profileBio = transform.FindChild(objectLabel+"Card").FindChild(objectLabel).gameObject;
 		profileBio.GetComponent<TextMesh> ().text = objectText;
 	}	
 
 	public string GetPhotoUrl() {
-		return profileModel.photo_url;
+		return _profileModel.photo_url;
 	}
 }
---
+++ Assets/Scripts/PreciseProfile/Selector/PreciseProfileSelector.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PreciseProfileSelector : MonoBehaviour {
+
+	private int _currentFilteredProfileIndex;
+	private List<PreciseProfileModel> _profileCollection = new List<PreciseProfileModel>();
+	private List<PreciseProfileModel> _filteredProfileCollection = new List<PreciseProfileModel>();
+	
+    private enum SelectorStates {
+		None,
+		SelectLeft,
+		SelectRight
+    }
+
+    private enum Directions {
+		Left = -1,
+		Right = 1
+    }
+
+    private const string FILTER_ALL = "ALL";
+    private const string FILTER_MARKETING_CONSULTANT = "Marketing Consultant";
+
+	private SelectorStates _selectorState;
+
+	public void Start () {
+
+		_selectorState = SelectorStates.None;
+	}
+
+    private void UpdateProfilePicture() {
+		var tex = _filteredProfileCollection[_currentFilteredProfileIndex].profilePictureTex;
+		var profilePicture = transform.FindChild("ProfilePicture").gameObject;
+		profilePicture.GetComponent<Renderer> ().material.mainTexture = tex;
+	}
+
+	public void SelectLeft() {
+		_selectorState = SelectorStates.SelectLeft;
+	}
+
+	public void SelectRight() {
+		_selectorState = SelectorStates.SelectRight;
+	}
+
+    private void Update () {
+		switch(_selectorState) {
+			case SelectorStates.None:
+				break;
+			case SelectorStates.SelectLeft:
+				if(_profileCollection.Count > 0) {
+					UpdateDisplayedProfile((int)Directions.Left);
+				}
+				_selectorState = SelectorStates.None;
+				break;
+			case SelectorStates.SelectRight:
+				if(_profileCollection.Count > 0) {
+					UpdateDisplayedProfile((int)Directions.Right);
+				}
+				_selectorState = SelectorStates.None;
+				break;
+		    default:
+		        throw new ArgumentOutOfRangeException();
+		}
+	}
+
+    private void UpdateDisplayedProfile(int i)
+    {
+        _currentFilteredProfileIndex += i;
+		if (_currentFilteredProfileIndex >= _filteredProfileCollection.Count) {
+			_currentFilteredProfileIndex = _filteredProfileCollection.Count - 1;
+		} else if (_currentFilteredProfileIndex <= 0) {
+			_currentFilteredProfileIndex = 0;
+		}
+        UpdateProfilePicture();
+    }
+
+    private void UpdateDisplay(GameObject gameObject) {
+		if(gameObject.name == "RightButton") {
+			_selectorState = SelectorStates.SelectRight;
+		} else {
+			_selectorState = SelectorStates.SelectLeft;
+		}
+    }
+
+	void OnEnable() {
+		PreciseProfileController.Direction += UpdateDisplay;
+		PreciseProfileController.AddProfile += AddProfile;
+		PreciseProfileController.DeleteProfile += DeleteProfile;
+		PreciseProfileService.PassProfileModels += LoadSelectorWithProfiles;
+	}
+
+    void OnDisable() {
+		PreciseProfileController.Direction -= UpdateDisplay;
+		PreciseProfileController.AddProfile -= AddProfile;
+		PreciseProfileController.DeleteProfile -= DeleteProfile;
+		PreciseProfileService.PassProfileModels -= LoadSelectorWithProfiles;
+	}
+
+    private void DeleteProfile(Transform transform) {
+        PreciseProfileService.DeleteProfile(transform);
+    }
+
+    private void AddProfile() {
+        if (_filteredProfileCollection.Count > 0)
+        {
+            PreciseProfileService.AddProfile(_filteredProfileCollection[_currentFilteredProfileIndex]);
+        }
+    }
+
+	private void LoadSelectorWithProfiles(List<PreciseProfileModel> aProfileCollection) {
+	    _profileCollection = aProfileCollection;
+	    _filteredProfileCollection = FilterSelectorProfiles(FILTER_ALL);
+	    UpdateProfilePicture ();
+	}
+
+	private List<PreciseProfileModel> FilterSelectorProfiles(string aTitleFilter) {
+	    if(aTitleFilter == FILTER_ALL) {
+            return _profileCollection;
+        }
+	    // reset index to avoid out of bounds index
+        _currentFilteredProfileIndex = 0;
+        return _profileCollection.Where(p => p.title == aTitleFilter).ToList();
+    }
+}
